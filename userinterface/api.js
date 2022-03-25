f09135b1592c096740ad43b5d29b87b8b0e11fef
srikanthsista <srikanthsista304@gmail.com>
2022-03-25 13:47:38 -0400

    Url Change

--- userinterface/api.js
+++ userinterface/api.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
-const BASE_URL = 'http://127.0.0.1:5000/'
+const BASE_URL = 'http://18.209.23.179:5000/'
 export const parseQuery = (database, schema, query) => axios.post(BASE_URL+`query?database=${database}&query=${query}&schema=${schema}`);
 
